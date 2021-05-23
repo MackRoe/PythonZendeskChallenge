0dfcb8555d366824379fd400ed505c31d2533774
MackRoe <elaine.music@students.makeschool.com>
2021-05-23 10:04:00 -0400

    begin Command Line User Interface (CLUI.py)

---
+++ clui.py
@@ -0,0 +1,19 @@
+# Commang Line User Interface
+
+class CLUI:
+    def __init__(self):
+        pass
+
+    def display_choices(self):
+        print("Choice 1 (Enter: 1)")
+        print("Choice 1 (Enter: 1)")
+        print("Choice 1 (Enter: 1)")
+
+
+    def get_input(self):
+        user_action_choice = input("")
+
+        pass
+
+    def return_output(self):
+        pass
--- tickets.py
+++ tickets.py
@@ -4,12 +4,20 @@ from dotenv import load_dotenv
 # Import .env variables
 load_dotenv()
 
-# Parameters
-url = 'https://d3v-mackroetech.zendesk.com'
+# Request Parameters
+url = 'https://d3v-mackroetech.zendesk.com/api/v2/'
 user_email = USER_EMAIL
 sec_key = ZENDESK_PASSWORD
 
+endpoint_filename = ''
 
-r = requests.get(url, params = PARAMS)
+# HTTP get request
+res = requests.get(url, auth=(user, pwd))
 
-data = r.json()
+# Check for HTTP codes other than 200
+if response.status_code != 200;
+    print('Status:', response.status_code, 'Unsuccessful Page Load: Aborted.')
+    exit()
+
+# JSON response convert to dictionary
+data = res.json()
